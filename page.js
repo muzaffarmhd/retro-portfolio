ee75f9c70561e6f9a0e170efc71c4e3d5527af7f
Muzaffar <mdmuzaffarprof@gmail.com>
2024-03-01 12:03:07 +0530

    added animations

--- page.js
+++ page.js
@@ -4,9 +4,27 @@ window.onscroll = function() {
     if (scrolled > 100) {
         nav.style.backgroundColor = '#73a582';
         nav.style.zIndex = '999';
-        nav.style.opacity = '0.7';
+        nav.style.opacity = '0.9';
     } else {
         nav.style.backgroundColor = '';
         nav.style.color = 'black';
     }
 };
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add('in-view');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+const hiddenImage = document.querySelectorAll('.hiddenimg');
+hiddenElements.forEach(element => {
+    observer.observe(element);
+});
+hiddenImage.forEach(element => {
+    observer.observe(element);
+});
